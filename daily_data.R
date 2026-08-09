2ac15b84c7c9eb591d9be150e36693f04e252e5e
ma-barbour <111284500+ma-barbour@users.noreply.github.com>
2026-08-09 17:19:43 -0400

    added volume data

--- daily_data.R
+++ daily_data.R
@@ -31,14 +31,17 @@ price_data <- price_data |>
         mutate(daily_return = (adjusted / lag(adjusted)) - 1,
                ma_20  = SMA(adjusted, n = 20),
                ma_50  = SMA(adjusted, n = 50),
-               ma_200 = SMA(adjusted, n = 200)) |>
-        ungroup()
+               ma_200 = SMA(adjusted, n = 200),
+               vol_sma5   = SMA(volume, n = 5),
+               log_vol    = log(vol_sma5),
+               vol_zscore = (log_vol - SMA(log_vol, n = 50)) / runSD(log_vol, n = 50)) |>
+        ungroup() |>
+        select(-vol_sma5, -log_vol)
 
 price_data <- price_data |>
         mutate(daily_return = round(daily_return, 4) * 100,
+               vol_zscore   = round(vol_zscore, 4),
                across(c(adjusted, ma_20:ma_200), \(x) round(x, digits = 2)))
-
-# Add a PCA analysis
 
 wide_returns <- price_data |>
         select(date, symbol, daily_return) |>
@@ -71,14 +74,42 @@ pca_list <- list(scores = pca_scores,
                  loadings = pca_loadings,
                  variance_explained = pca_variance)
 
+wide_vol <- price_data |>
+        filter(symbol != "XEG") |> 
+        select(date, symbol, vol_zscore) |>
+        pivot_wider(names_from = symbol, values_from = vol_zscore) |>
+        drop_na()
 
+pca_vol_data <- wide_vol |>
+        filter(date >= (max(date) - 90))
 
-# JSON
+pca_vol_fit <- prcomp(pca_vol_data |> select(-date), center = TRUE, scale. = TRUE)
+
+pca_vol_scores <- pca_vol_data |>
+        select(date) |>
+        bind_cols(as_tibble(pca_vol_fit$x)) |>
+        mutate(across(where(is.numeric), \(x) round(x, 4)))
+
+pca_vol_loadings <- as_tibble(pca_vol_fit$rotation, rownames = "symbol") |>
+        mutate(across(where(is.numeric), \(x) round(x, 4)))
+
+if (sum(pca_vol_loadings$PC1) < 0) {
+        
+        pca_vol_scores$PC1   <- pca_vol_scores$PC1 * -1
+        pca_vol_loadings$PC1 <- pca_vol_loadings$PC1 * -1
+        
+}
+
+pca_vol_variance <- as.list(summary(pca_vol_fit)$importance[2, ])
+
+pca_vol_list <- list(scores = pca_vol_scores,
+                     loadings = pca_vol_loadings,
+                     variance_explained = pca_vol_variance)
 
 app_data <- list(is_complete = complete_check,
                  last_date = date_check,
                  prices = price_data,
-                 pca = pca_list)
+                 pca = pca_list,
+                 pca_volume = pca_vol_list) 
 
 write_json(app_data, "app_data.json", pretty = TRUE, auto_unbox = TRUE)
-
